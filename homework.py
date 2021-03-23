79ca68665e50181cd15b2787a5230251a698464f
KhantsevDanil <khantsev.danil@gmail.com>
2021-03-23 22:43:37 +0300

    1.2 commit dor sprint 8

--- homework.py
+++ homework.py
@@ -16,7 +16,7 @@ load_dotenv()
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-YANDEX_HOMEWORK_STATUS =(
+YANDEX_HOMEWORK_STATUS = (
     'https://praktikum.yandex.ru/api/user_api/homework_statuses/')
 bot_client = telegram.Bot(token=TELEGRAM_TOKEN)
 
@@ -36,7 +36,7 @@ def parse_homework_status(homework):
     if homework["status"] == "rejected":
         verdict = 'К сожалению в работе нашлись ошибки.'
     else:
-        verdict =(
+        verdict = (
             'Ревьюеру всё понравилось, можно приступать к следующему уроку.')
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
@@ -51,7 +51,7 @@ def get_homework_statuses(current_timestamp):
         и ошибку не правильного формата json.
         При возникновении какой либо ошибки в телеграм бот выведет сообщение,
         соотвутствующее ошиьке"""
-    server_name =(
+    server_name = (
         'https://praktikum.yandex.ru/api/user_api/homework_statuses/')
     headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
     params = {'from_date': current_timestamp}
@@ -92,8 +92,10 @@ def main():
         try:
             new_homework = get_homework_statuses(current_timestamp)
             if new_homework.get('homeworks'):
-                send_message(parse_homework_status(new_homework.get('homeworks')[0]))
-            current_timestamp = new_homework.get('current_date', current_timestamp)
+                send_message(parse_homework_status(
+                    new_homework.get('homeworks')[0]))
+            current_timestamp = new_homework.get(
+                'current_date', current_timestamp)
             time.sleep(300)
 
         except Exception as e:
